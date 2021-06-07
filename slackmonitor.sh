9c8ee256ecd68413b6156d314255917e64853143
jwisniewski-aws <85501549+jwisniewski-aws@users.noreply.github.com>
2021-06-07 07:26:31 -0700

    Add files via upload

---
+++ slackmonitor.sh
@@ -0,0 +1,40 @@
+
+#!/bin/bash -x
+
+
+# url of the site that that you are monitoring.
+slackwebhook=https://
+url=http://
+
+
+# slack messaging function.
+
+slack_msg () {
+
+
+msg=$1
+
+
+curl -X POST --data-urlencode "payload={\"channel\": \"#monitoring\", \"username\": \"Jarvis\", \"text\": \"${msg}\", \"icon_emoji\": \":robot_face:\"}" ${slackwebhook}
+
+
+}
+
+
+# Curl function to pull the status code of the designated site.
+
+get_status () {
+
+
+   status_code=$(curl -sI ${url} | head -n 1 | awk '{print $2}')
+      if [ "${status_code}" == "200" ]
+   then 
+      slack_msg "Site is working. ${status_code} response code returned"
+   else
+      slack_msg "Site is returning a ${status_code}. Something is not right."
+fi
+
+}
+
+# function call.
+get_status 
